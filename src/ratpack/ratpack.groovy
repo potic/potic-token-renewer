c41b090e4ea73ad5de2a8aeac0fe5e452ab6ed85
Yaroslav Yermilov <yermilov17@gmail.com>
2017-11-27 01:03:43 +0200

    try deprecated token renew method

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -5,9 +5,17 @@ String environmentName = System.getenv('ENVIRONMENT_NAME') ?: 'dev'
 Properties applicationProperties = new Properties()
 applicationProperties.load(this.class.getResourceAsStream("application-${environmentName}.properties"))
 
+String auth0Domain = applicationProperties.get('auth0Domain')
+String auth0ClientId = applicationProperties.get('auth0ClientId')
+String auth0Scope = applicationProperties.get('auth0Scope')
+String auth0Response = applicationProperties.get('auth0Response')
 String callbackUrl = applicationProperties.get('callbackUrl')
 
 String renewTokenHtml = this.class.getResourceAsStream('renew-token.html').text
+renewTokenHtml = renewTokenHtml.replace('__AUTH0_DOMAIN__', auth0Domain)
+renewTokenHtml = renewTokenHtml.replace('__AUTH0_CLIENT_ID__', auth0ClientId)
+renewTokenHtml = renewTokenHtml.replace('__AUTH0_SCOPE__', auth0Scope)
+renewTokenHtml = renewTokenHtml.replace('__AUTH0_RESPONSE__', auth0Response)
 renewTokenHtml = renewTokenHtml.replace('__CALLBACK_URL__', callbackUrl)
 
 ratpack {
